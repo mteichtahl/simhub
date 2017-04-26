3629a26a40884cc2cb9a19d33fc2461864a50144
Marc Teichtahl <marc@teichtahl.com>
2017-04-27 03:43:07 +1000

    initial cli scaffolding

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,24 @@
 #include <vector>
 
 #include "configManager/CConfigManager.h"
+#include "libs/commandLine.h"
 #include "log/CLog.h"
 
 using namespace std;
 
-int main() {
-    CConfigManager config("config/config.cfg");
+void configureCli(cmdline::parser* cli) {
+    cli->add<string>("config", 'c', "config file", false, "config/config.cfg");
+
+    cli->set_program_name("simhub");
+    cli->footer("\n");
+}
+
+int main(int argc, char* argv[]) {
+    cmdline::parser cli;
+    configureCli(&cli);
+    cli.parse_check(argc, argv);
+
+    CConfigManager config(cli.get<string>("config"));
 
     return 0;
 }
