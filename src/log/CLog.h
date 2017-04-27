4b085f855f4185cd6a84fb752e53e5b242b1277e
Marc Teichtahl <marc@teichtahl.com>
2017-04-27 17:15:39 +1000

    style alignement

--- src/log/CLog.h
+++ src/log/CLog.h
@@ -7,37 +7,31 @@
 #include <string.h>
 #include <zlog.h>
 
-using namespace std;
-
-enum logCategory {
-    LOG_INFO  = 1,
-    LOG_ERROR = 2,
-    LOG_DEBUG = 3
-};
+enum logCategory { LOG_INFO = 1, LOG_ERROR = 2, LOG_DEBUG = 3 };
 
 #define MAX_VA_LENGTH 4096
 
 /**
-  *  @brief Class allowing for logging to various targets based on zlog.  
-  */
+ *  @brief Class allowing for logging to various targets based on zlog.
+ */
 class CLog {
 public:
     // Default constructor
     CLog();
     // Destructor
     ~CLog();
-    void init(string configFilename);
+    void init(std::string configFilename);
     // log @msg to specific @category - uses variadic parameters
-    void log(const int category, const char* msg, ...);
+    void log(const int category, const char *msg, ...);
 
 protected:
-    bool canZlog;                   ///< true when using zlog, cout otherwise
-    const char* configFilename;     ///< location of the zlong configuration file
-    zlog_category_t* infoCategory;  ///< a zlog category for informational logging
-    zlog_category_t* errorCategory; ///< a zlog category for error logging
-    zlog_category_t* debugCategory; ///< a zlog category for error logging
-    int zlog;                       ///< the zlog handler
-    int maxVA_length;               ///< maximum length (in char) of the log method variadic parameters
+    bool canZlog; ///< true when using zlog, cout otherwise
+    const char *configFilename; ///< location of the zlong configuration file
+    zlog_category_t *infoCategory; ///< a zlog category for informational logging
+    zlog_category_t *errorCategory; ///< a zlog category for error logging
+    zlog_category_t *debugCategory; ///< a zlog category for error logging
+    int zlog; ///< the zlog handler
+    int maxVA_length; ///< maximum length (in char) of the log method variadic parameters
 };
 
 extern CLog logger; ///< allow externals to access logger
--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,16 @@
 #include "libs/commandLine.h" // https://github.com/tanakh/cmdline
 #include "log/CLog.h"
 
-void configureCli(cmdline::parser* cli)
+void configureCli(cmdline::parser *cli)
 {
-    cli->add<string>("config", 'c', "config file", false, "config/config.cfg");
-    cli->add<string>("logConfig", 'l', "log config file", false, "config/zlog.conf");
+    cli->add<std::string>("config", 'c', "config file", false, "config/config.cfg");
+    cli->add<std::string>("logConfig", 'l', "log config file", false, "config/zlog.conf");
 
     cli->set_program_name("simhub");
     cli->footer("\n");
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     cmdline::parser cli;
     configureCli(&cli);
