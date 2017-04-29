d124fcd142b8b146c46128ae96ebd28d2ed8f31a
Marten Payne <martenp@amazon.com>
2017-04-29 20:30:12 +1000

    Added some comments to the test main file

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -1,15 +1,17 @@
 #include <gtest/gtest.h>
+
+// -- these are examples, showing the general test syntax
  
 TEST(SquareRootTest, PositiveNos) 
 { 
 }
  
-TEST (SquareRootTest, ZeroAndNegativeNos) 
+TEST(SquareRootTest, ZeroAndNegativeNos) 
 { 
 }
  
 int main(int argc, char **argv) 
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
