7233ae0316b2ad5264a920d410e8a09a3555a5e9
Marc Teichtahl <marc@teichtahl.com>
2017-05-09 10:55:33 +1000

    plugins

--- src/common/elements/attributes/attribute.h
+++ src/common/elements/attributes/attribute.h
@@ -1,21 +1,27 @@
 #ifndef __ATTRIBUTE_H
 #define __ATTRIBUTE_H
 
+//#include <variant>
 #include <string>
 
 typedef enum { INT_ATTRIBUTE = 0, FLOAT_ATTRIBUTE, STRING_ATTRIBUTE, BOOL_ATTRIBUTE } eAttribute_t;
 class Attribute
 {
 protected:
+    //std::variant<int64_t, float, double, bool, std::string> _value;
+
 public:
-    Attribute();
+    Attribute(void);
+
+   // template <typename T> virtual T getValue(void);
+
     std::string _name;
     std::string _description;
     eAttribute_t _type;
     std::string _defaultValue;
+    std::string _value;
     const char *toString();
     std::string getValue() { return _value; };
-    std::string _value;
 };
 
 #endif
