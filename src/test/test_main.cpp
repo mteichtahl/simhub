5c06b8d44d3440a7587fff7070924ffa20745b80
Marc Teichtahl <marc@teichtahl.com>
2017-05-09 21:27:47 +1000

    improved test structure
    Signed-off-by: Marc Teichtahl <marc@teichtahl.com>

--- src/test/test_main.cpp
+++ src/test/test_main.cpp
@@ -1,16 +1,6 @@
-#include <gtest/gtest.h>
+#include "test_logging.h"
 
-// -- these are examples, showing the general test syntax
- 
-TEST(SquareRootTest, PositiveNos) 
-{ 
-}
- 
-TEST(SquareRootTest, ZeroAndNegativeNos) 
-{ 
-}
- 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
