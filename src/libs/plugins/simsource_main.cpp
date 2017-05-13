2e7973e8bcc496a92ee25c0f0e02eabfae7892a7
Marc Teichtahl <marc@teichtahl.com>
2017-05-13 19:02:22 +1000

    introduce sim source for the plugins
    Signed-off-by: Marc Teichtahl <marc@teichtahl.com>

---
+++ src/libs/plugins/simsource_main.cpp
@@ -0,0 +1,84 @@
+#include <assert.h>
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+#include "simsource_main.h"
+
+class PluginStateManager
+{
+protected:
+    virtual void runTestEventLoop(void);
+    EnqueueEventHandler _enqueueCallback;
+    std::thread _testEventThread;
+
+public:
+    PluginStateManager(void);
+    virtual ~PluginStateManager(void);
+
+    virtual void commenceEventing(EnqueueEventHandler enqueueCallback);
+    virtual void ceaseEventing(void);
+};
+
+PluginStateManager::PluginStateManager(void)
+    : _enqueueCallback(NULL)
+{
+    std::cout << "simplugin init'd" << std::endl;
+    _testEventThread = std::thread(std::bind(&PluginStateManager::runTestEventLoop, this));
+}
+
+PluginStateManager::~PluginStateManager(void)
+{
+    std::cout << "simplugin release" << std::endl;
+    _testEventThread.join();
+}
+
+void PluginStateManager::runTestEventLoop(void)
+{
+    std::cout << "generating 9 events in a sleep loop" << std::endl;
+    for (size_t i = 0; i < 9; i++) {
+        sleep(1);
+        _enqueueCallback(this, (void *)"echo");
+    }
+    std::cout << "done" << std::endl;
+}
+
+void PluginStateManager::commenceEventing(EnqueueEventHandler enqueueCallback)
+{
+    std::cout << "commence eventing" << std::endl;
+    _enqueueCallback = enqueueCallback;
+    enqueueCallback(this, NULL);
+}
+
+void PluginStateManager::ceaseEventing(void)
+{
+    std::cout << "cease eventing" << std::endl;
+
+    _enqueueCallback(this, NULL);
+}
+
+// -- public C FFI
+
+extern "C" {
+int simplug_init(SPHANDLE *plugin_instance)
+{
+    *plugin_instance = new PluginStateManager();
+    return 0;
+}
+
+void simplug_commence_eventing(SPHANDLE plugin_instance, EnqueueEventHandler enqueue_callback)
+{
+    static_cast<PluginStateManager *>(plugin_instance)->commenceEventing(enqueue_callback);
+}
+
+void simplug_cease_eventing(SPHANDLE plugin_instance)
+{
+    static_cast<PluginStateManager *>(plugin_instance)->ceaseEventing();
+}
+
+void simplug_release(SPHANDLE plugin_instance)
+{
+    assert(plugin_instance);
+    delete static_cast<PluginStateManager *>(plugin_instance);
+}
+}
