e174d391f0a941e101a3b9ba54eb3e174682d8ff
Marc Teichtahl <marc@teichtahl.com>
2017-08-08 14:35:56 +1000

    adding a UINT_ATTRIBUTE attribute

--- src/common/elements/attributes/attribute.h
+++ src/common/elements/attributes/attribute.h
@@ -7,7 +7,7 @@
 #include <string>
 #include <variant.hpp>
 
-typedef enum { INT_ATTRIBUTE = 0, FLOAT_ATTRIBUTE, STRING_ATTRIBUTE, BOOL_ATTRIBUTE } eAttribute_t;
+typedef enum { INT_ATTRIBUTE = 0, FLOAT_ATTRIBUTE, STRING_ATTRIBUTE, BOOL_ATTRIBUTE, UINT_ATTRIBUTE } eAttribute_t;
 
 class Attribute
 {
@@ -23,34 +23,37 @@ public:
 
     Attribute(void);
     virtual ~Attribute(void);
-    
+
     template <typename T> void setValue(T value)
     {
         _value = value;
         _timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
     };
     template <typename T> T getValue(void) { return mpark::get<T>(_value); };
-    
+
     std::string getValueToString(void)
     {
         std::ostringstream oss; // create a stream
 
         switch (_type) {
-            case INT_ATTRIBUTE:
-                oss << mpark::get<int>(_value); // insert value to stream
-                break;
-            case FLOAT_ATTRIBUTE:
-                oss << mpark::get<float>(_value); // insert value to stream
-                break;
-            case STRING_ATTRIBUTE:
-                oss << mpark::get<std::string>(_value); // insert value to stream
-                break;
-            case BOOL_ATTRIBUTE:
-                oss << mpark::get<bool>(_value); // insert value to stream
-                break;
-            default:
-                assert(false);
-                break;
+        case INT_ATTRIBUTE:
+            oss << mpark::get<int>(_value); // insert value to stream
+            break;
+        case FLOAT_ATTRIBUTE:
+            oss << mpark::get<float>(_value); // insert value to stream
+            break;
+        case STRING_ATTRIBUTE:
+            oss << mpark::get<std::string>(_value); // insert value to stream
+            break;
+        case UINT_ATTRIBUTE:
+            oss << mpark::get<int>(_value); // insert value to stream
+            break;
+        case BOOL_ATTRIBUTE:
+            oss << mpark::get<bool>(_value); // insert value to stream
+            break;
+        default:
+            assert(false);
+            break;
         }
         return oss.str(); // extract value and return
     }
