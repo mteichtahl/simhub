4ede570cc89e1d1b98498dd84b5c592bd639d61a
Marc Teichtahl <marc@teichtahl.com>
2017-08-20 17:41:21 +1000

    lambda function cleans up the payload provided by kinesis

--- tools/aws/cfn/stacks/lambda/functions/processKinesis/handler.js
+++ tools/aws/cfn/stacks/lambda/functions/processKinesis/handler.js
@@ -2,13 +2,41 @@ var AWS = require('aws-sdk');
 var _ = require('lodash');
 var base64 = require('base-64');
 
-exports.index = function(event, context, callback) {
-  var recordCount = event.Records.length;
-  console.log(recordCount);
+/**
+ * cleanJSONString
+ *
+ * @param {any} string string that requires cleaning
+ * @returns string cleaned string
+ */
+function cleanJSONString(string) {
+  // preserve newlines, etc - use valid JSON
+  string = string.replace(/\\n/g, '\\n')
+               .replace(/\\'/g, '\\\'')
+               .replace(/\\"/g, '\\"')
+               .replace(/\\&/g, '\\&')
+               .replace(/\\r/g, '\\r')
+               .replace(/\\t/g, '\\t')
+               .replace(/\\b/g, '\\b')
+               .replace(/\\f/g, '\\f');
+  // remove non-printable and other non-valid JSON chars
+  return string.replace(/[\u0000-\u0019]+/g, '');
+}
 
+/**
+ * Main lambda handler and entry point
+ */
+exports.index = function(event, context, callback) {
+  // find the number of records being given to us by kinesis
+  var recordCount = event.Records.length;
+
+  // iterate through the records
   for (i = 0; i < recordCount; i++) {
+    // get the kinesis payload
     var kinesis = event.Records[i].kinesis;
-    data = base64.decode(kinesis.data);
+
+    // convert from base64, clean up and "funny" characters and parse into JSON
+    data = JSON.parse(cleanJSONString(base64.decode(kinesis.data)));
+
     console.log(data);
   }
 
