7c12526eaf3424c6550de1b4b17cf0c55ac50392
Marc Teichtahl <marc@teichtahl.com>
2017-08-20 19:35:09 +1000

    Kinesis processing lambda function

--- tools/aws/cfn/stacks/lambda/functions/processKinesis/handler.js
+++ tools/aws/cfn/stacks/lambda/functions/processKinesis/handler.js
@@ -1,6 +1,11 @@
 var AWS = require('aws-sdk');
 var _ = require('lodash');
 var base64 = require('base-64');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+var cloudwatch = new AWS.CloudWatch();
+var dynamodb = new AWS.DynamoDB();
 
 /**
  * cleanJSONString
@@ -22,6 +27,48 @@ function cleanJSONString(string) {
   return string.replace(/[\u0000-\u0019]+/g, '');
 }
 
+function writeToDynamo(name, value) {
+  var params = {
+    Item: {'source': {S: name}, 'Value': {S: value}},
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: 'simhub'
+  };
+
+  dynamodb.putItem(params, function(err, data) {
+    if (err)
+      console.log(err, err.stack);  // an error occurred
+    else
+      console.log(data);  // successful response
+  });
+}
+
+function putCloudwatchMetric(metricName, dimName, dimValue, ts, value) {
+  var params = {
+    MetricData: [
+      /* required */
+      {
+        MetricName: metricName,
+        Dimensions: [
+          {Name: dimName, Value: dimValue},
+        ],
+        StorageResolution: 1,
+        Timestamp: new Date(ts).toISOString(),
+        Unit: 'None',
+        Value: parseFloat(value)
+      },
+
+    ],
+    Namespace: 'simhub' /* required */
+  };
+
+  cloudwatch.putMetricData(params, function(err, data) {
+    if (err)
+      console.log(err, err.stack);  // an error occurred
+    else
+      console.log(data);  // successful response
+  });
+}
+
 /**
  * Main lambda handler and entry point
  */
@@ -34,12 +81,14 @@ exports.index = function(event, context, callback) {
     // get the kinesis payload
     var kinesis = event.Records[i].kinesis;
 
-    // convert from base64, clean up and "funny" characters and parse into JSON
+    // convert from base64, clean up and "funny" characters and parse into
+    // JSON
     data = JSON.parse(cleanJSONString(base64.decode(kinesis.data)));
 
-    console.log(data);
+    putCloudwatchMetric(data.s, data.s, 'On/Off', data.ts, data.val);
+    writeToDynamo(data.s, data.val);
   }
 
 
-  callback(null, 'Hello from Lambda');
+  callback(null, 'Complete');
 }
